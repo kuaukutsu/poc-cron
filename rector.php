0bc6997159b156da87686122efa4123d7704152a
Dmitriy Krivopalov <dimarik82@gmail.com>
2024-02-24 05:48:41 +0300

    composer update 20240224

--- rector.php
+++ rector.php
@@ -10,7 +10,6 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths(
         [
             __DIR__ . '/src',
-            __DIR__ . '/tests',
         ]
     );
 
@@ -18,8 +17,12 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
 
     // define sets of rules
-    $rectorConfig->sets([
-        SetList::PHP_81,
-        SetList::PHP_82,
-    ]);
+    $rectorConfig->sets(
+        [
+            SetList::CODE_QUALITY,
+            SetList::DEAD_CODE,
+            SetList::PHP_81,
+            SetList::PHP_82,
+        ]
+    );
 };
